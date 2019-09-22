7e95ac9928d5075f13a5a981f7644a9ea6a6492a
laddi <meladbrook@gmail.com>
2019-09-22 11:36:32 -0700

    Made pig_latin.py more interactive

--- pig_latin.py
+++ pig_latin.py
@@ -1,8 +1,19 @@
-"""Takes a word and forms it's pig latin equivalent"""
+"""Takes a word and forms it's pig latin equivalent."""
 
-def pigify(word):
-    """Takes first letter of word, adds it to the end, and adds 'ay'."""
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    if word[0] not in vowels:
-        return word[1:] + word[0] + 'ay'
-    return word + 'way'
+def pigify():
+    """Take first letter of word, adds it to the end, and adds 'ay'."""
+    while True:
+        word = input("Choose a word to pigify\n")
+        print("\n\n")
+        vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+        if word[0] not in vowels:
+            print(word[1:] + word[0] + 'ay')
+        else:
+            print(word + 'way')
+        try_again = input("\nAgain?\n")
+        if try_again.lower() == "n":
+            break
+    input("\nPress Enter to exit.")
+
+if __name__ == "__main__":
+    pigify()
