743d013f16cd3b1a170f680c984461dc59d56843
laddi <meladbrook@gmail.com>
2019-09-27 22:03:28 -0700

    Fixed poor_man_bar_chart.py

--- poor_man_bar_chart.py
+++ poor_man_bar_chart.py
@@ -1,20 +1,22 @@
 """Create a simple bar chart to show number of each letter in a sentence."""
 
-from _collections import defaultdict
 from pprint import pprint
+from _collections import defaultdict
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 def gather_letters():
     """Create a bar chart of letters from a input sentence."""
     while True:
         sentence = input("Type a sentence: \n")
         print("\n")
-        letters = defaultdict(int)
+        letters = defaultdict(list)
         for letter in sentence:
-            letters[letter] += 1
-        pprint(letters, width=1)
+            letter = letter.lower()
+            if letter in ALPHABET:
+                letters[letter].append(letter)
+        pprint(letters, width=110)
 
 
 if __name__ == "__main__":
     gather_letters()
-
