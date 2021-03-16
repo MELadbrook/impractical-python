9e2b5acef6f2e50f1bb337d363b390a931d05667
laddi <meladbrook@gmail.com>
2021-03-15 22:10:12 -0700

    Added document info to route_cipher_decrypt_prototype.py

--- Chapter 4/route_cipher_decrypt_prototype.py
+++ Chapter 4/route_cipher_decrypt_prototype.py
@@ -1,13 +1,58 @@
-ciphertext = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"
+"""Decrypt a path through a Union Route Cipher.
+
+Designed for whole-word transposition ciphers with variable rows & columns.
+Assumes encryption began at either top or bottom of a column.
+Key indicates the order to read columns and the direction to traverse.
+Negative column numbers mean start at bottom and read up.
+Positive column numbers mean start at top & read down.
+
+Example below is for 4x4 matrix with key -1 2 -3 4.
+Note "0" is not allowed.
+Arrows show encryption route; for nagative key values read UP.
+
+  1   2   3   4
+ ___ ___ ___ ___
+| ^ | | | ^ | | | MESSAGE IS WRITTEN
+|_|_|_v_|_|_|_v_|
+| ^ | | | ^ | | | ACROSS EACH ROW
+|_|_|_v_|_|_|_v_|
+| ^ | | | ^ | | | IN THIS MANNER
+|_|_|_v_|_|_|_v_|
+| ^ | | | ^ | | | LAST ROW IS FILLED WITH DUMMY WORDS
+|_|_|_v_|_|_|_v_|
+START        END
+
+Required inputs - a text message, # of columns, # of rows, key string
+
+Prints translated plaintext
+"""
+import sys
+
+#=======================================================================
+# USER INPUT:
+
+# the string to be decrypted (type or paste between the triple-quotes):
+ciphertext = """16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"""
+
+# number of columns in the transposition matrix:
+COLS = 4
+
+# number of rows in the transposition matrix:
+ROWS = 5
+
+# key with spaces between numbers; negative to read UP column (ex = -1 2 -3 4):
+key = """-1 2 -3 4"""
+
+# END OF USER INPUT
+#======================================================================
 
 # split elements into words, not letters
 
 cipherlist = list(ciphertext.split())
 
 # initialise variables
-COLS = 4
-ROWS = 5
-key = '-1 2 -3 4' # neg number means read UP column vs. DOWN
+
+
 translation_matrix = [None] * COLS
 plaintext = ''
 start = 0
