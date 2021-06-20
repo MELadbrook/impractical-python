8399562ca6b10dba8e7d5c2355d8b352ffd7078a
meladbrook <meladbrook@gmail.com>
2021-06-20 14:45:27 -0700

    added sorting function to super_rats.py

--- Chapter 7/super_rats.py
+++ Chapter 7/super_rats.py
@@ -18,3 +18,28 @@ GENERATION_LIMIT = 500
 # ensure even-number of rats for breeding pairs:
 if NUM_RATS % 2 != 0:
     NUM_RATS += 1
+
+
+def populate(num_rats, min_wt, max_wt, mode_wt):
+    """Initialise a population with a trinagular distribution of weights."""
+    return [int(random.triangular(min_wt, max_wt, mode_wt))\
+            for i in range(num_rats)]
+
+
+def fitness(population, goal):
+    """Measure population fitness based on an attribute mean vs target."""
+    ave = statistics.mean(population)
+    return ave / goal
+
+
+def select(population, to_retain):
+    """Cull a population to retain only a specified number of members."""
+    sorted_population = sorted(population)
+    to_retain_by_sex = to_retain//2
+    members_per_sex = len(sorted_population)//2
+    females = sorted_population[:members_per_sex]
+    males = sorted_population[members_per_sex:]
+    selected_females = females[-to_retain_by_sex:]
+    selected_males = males[-to_retain_by_sex:]
+    return selected_males, selected_females
+
