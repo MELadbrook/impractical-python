dadb4dad7fa1120c524b1bf225b8860526b671c7
meladbrook <meladbrook@gmail.com>
2021-08-08 22:02:03 -0700

    added word mapping functions to markov_haiku.py

--- Chapter 9/markov_haiku.py
+++ Chapter 9/markov_haiku.py
@@ -19,3 +19,30 @@ def prop_training(raw_haiku):
     """Load string, remove newline, split words on spaces, and return list."""
     corpus = raw_haiku.replace('\n', ' ').split()
     return corpus
+
+
+def map_word_to_word(corpus):
+    """Load list & use dictionary to map word to word that follows."""
+    limit = len(corpus) - 1
+    dict1_to_1 = defaultdict(list)
+    for index, word in enumerate(corpus):
+        if index < limit:
+            suffix = corpus[index + 1]
+            dict1_to_1[word].append(suffix)
+    logging.debug("map_word_to_word results for \"sake\" = %s\n",
+                  dict1_to_1['sake'])
+    return dict1_to_1
+
+
+def map_2_words_to_word(corpus):
+    """Load list & use dictionary to map word-pair to trailing word."""
+    limit = len(corpus) - 2
+    dict2_to_1 = defaultdict(list)
+    for index, word in enumerate(corpus):
+        if index < limit:
+            key = word + ' ' + corpus[index + 1]
+            suffix = corpus[index + 2]
+            dict2_to_1[key].append(suffix)
+    logging.debug("map_2_words_to_word results for \"sake jug\" = %s\n",
+                  dict2_to_1['sake jug'])
+    return dict2_to_1
