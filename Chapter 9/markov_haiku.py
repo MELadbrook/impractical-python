539624ee11afa81e12c49f1e6ebd65b339ca0569
meladbrook <meladbrook@gmail.com>
2021-08-15 12:27:15 -0700

    finished haiku building function in markov_haiku.py

--- Chapter 9/markov_haiku.py
+++ Chapter 9/markov_haiku.py
@@ -111,3 +111,39 @@ def haiku_line(suffix_map_1, suffix_map_2, corpus, end_prev_line, target_syls):
         if line_syls == target_syls:
             end_prev_line.extend(current_line[-2:])
             return current_line, end_prev_line
+        else:  # build lines 2 and 3
+            current_line.extend(end_prev_line)
+
+        while True:
+            logging.debug("line = %s\n", line)
+            prefix = current_line[-2] + ' ' + current_line[-1]
+            word_choices = word_after_double(prefix, suffix_map_2,
+                                             line_syls, target_syls)
+            while len(word_choices) == 0:
+                index = random.randint(0, len(corpus) - 2)
+                prefix = corpus[index] + ' ' + corpus[index + 1]
+                logging.debug("new random prefix = %s", prefix)
+                word_choices = word_after_double(prefix, suffix_map_2,
+                                                 line_syls, target_syls)
+            word = random.choice(word_choices)
+            num_syls = count_syllables(word)
+            logging.debug("word & syllables = %s %s", word, num_syls)
+
+            if line_syls + num_syls > target_syls:
+                continue
+            elif line_syls + num_syls < target_syls:
+                current_line.append(word)
+                line_syls += num_syls
+            elif line_syls + num_syls == target_syls:
+                current_line.append(word)
+                break
+
+    end_prev_line = []
+    end_prev_line.extend(current_line[-2:])
+
+    if line == '1':
+        final_line = current_line[:]
+    else:
+        final_line = current_line[2:]
+
+    return final_line, end_prev_line
